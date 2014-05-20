088e88939e8ebafd80fe0c3d7e1073f3e9b5ce3b
Fabricio PH <fabriph@gmail.com>
2014-05-20 15:43:15 -0300

    Cracking: Clarifications for problem 8

--- cracking-interview/chapter1/Problem8.java
+++ cracking-interview/chapter1/Problem8.java
@@ -1,12 +1,5 @@
 package chapter1;
 
 public class Problem8 {
-	// Create every possible rotation of S1, and then call isSubstring.
-	/*
-	 * So, there is a better way: So, we need to check if there's a way to split
-	 * s i into x and y such that xy = s i and yx = s2. Regardless of where the
-	 * division between x and y is, we can see that yx will always be a
-	 * substring of xyxy.That is,s2will always be a substring of s1s1. And this
-	 * is precisely how we solve the problem: simplydoisSubstringtslsl, s2).
-	 */
+	 MagicClass.isSubstring(s1 + s1, s2);
 }
