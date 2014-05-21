03c72f57d9663fadd36a3bbc6a3cdb3cb304bff3
Fabricio PH <fabriph@gmail.com>
2014-05-21 12:19:11 -0300

    Prime numbers library in python holds primes up to 1.000.000 dynamically loaded form a external text file

---
+++ project-euler/problems/PrimeNumbers.py
@@ -0,0 +1,54 @@
+import numpy
+
+class PrimeNumbersHolder:
+	MAX_COL = 1000
+	MAX_ROW = 10
+	STEP_GROWING_ROW = 5
+	matrix = None
+	def getInstance(self):
+		self.matrix = [[None for i in range(self.MAX_COL)] for j in range(self.MAX_ROW)]
+		current_col = 0
+		current_row = 0
+		for line in open('primesUpToOneMillion.txt','r'):
+			for x in line.split():
+				if current_col == self.MAX_COL:
+					current_col = 0
+					current_row += 1
+				if current_row == len(self.matrix):
+					self.matrix.extend([[None for i in range(self.MAX_COL)] for j in range(self.STEP_GROWING_ROW)])
+				self.matrix[current_row][current_col] = int(x)
+				current_col += 1
+		while self.matrix[-1][0] == None:
+			del self.matrix[-1]
+		while self.matrix[-1][-1] == None:
+			del self.matrix[-1][-1]
+
+	def binarySearchSecondDimension(self, list, number):
+		if len(list) == 1:
+			return number == list[0]
+		mid = len(list) / 2
+		if number < list[mid]:
+			return self.binarySearchSecondDimension(list[:mid], number)
+		else:
+			return self.binarySearchSecondDimension(list[mid:], number)
+
+	def binarySearchFirstDimension(self, start, end, number):
+		if start == end:
+			return self.binarySearchSecondDimension(self.matrix[start], number)
+		mid = (start + end) / 2
+		if number < self.matrix[mid][-1]:
+			return self.binarySearchFirstDimension(start, mid, number)
+		else:
+			return self.binarySearchFirstDimension(mid + 1, end, number)
+
+	def isPrime(self, number):
+		if number < 2:
+			return False
+		if self.matrix[-1][-1] < number:
+			return False # should raise an exception
+		return self.binarySearchFirstDimension(0, len(self.matrix) - 1, number)
+
+	def printed(self):
+		for x in range(0, len(self.matrix)):
+			print x, " ", self.matrix[x]
+		print
